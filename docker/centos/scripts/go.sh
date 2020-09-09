cc4206e47936b7d5e56798df89b8f1df3322eb79
jianshengqi <shengqi.jian@qq.com>
2020-09-09 14:44:36 +0800

    Update go.sh

--- docker/centos/scripts/go.sh
+++ docker/centos/scripts/go.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -eux
 
-GO="https://dl.google.com/go/go1.15.1.linux-amd64.tar.gz"
+export DEBIAN_FRONTEND=noninteractive
+
+GO="https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz"
 curl -o go.tgz $GO && tar xzf go.tgz -C /usr/local && rm -f go.tgz
 
 cat >> /etc/profile.d/go.sh <<EOF
@@ -9,13 +11,19 @@ cat >> /etc/profile.d/go.sh <<EOF
 export GOROOT=/usr/local/go
 export GOPATH=\${GOROOT}/mylib
 export GOPROXY=https://goproxy.cn
-export GO111MODULE=on
+export GO111MODULE=off
 
 go_bin_path=\${GOPATH}/bin:\${GOROOT}/bin
 if [ -n "\${PATH##*\${go_bin_path}}" -a -n "\${PATH##*\${go_bin_path}:*}" ]; then
     export PATH=\${PATH}:\${GOPATH}/bin:\${GOROOT}/bin
 fi
 EOF
+
+. /etc/profile
+
+# go-bindata
+go get -u github.com/jteeuwen/go-bindata/...
+mv $GOPATH/bin/go-bindata /usr/local/bin
 
 cat >> /root/.bashrc <<EOF
 . /etc/profile
