8e112f6519b7e2682b8a34a90ac18ffcdd66a103
jianshengqi <shengqi.jian@qq.com>
2020-09-09 14:03:04 +0800

    Update go.sh

--- docker/ubuntu/scripts/go.sh
+++ docker/ubuntu/scripts/go.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-GO="https://dl.google.com/go/go1.14.8.linux-amd64.tar.gz"
+GO="https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz"
 curl -o go.tgz $GO && tar xzf go.tgz -C /usr/local && rm -f go.tgz
 
 cat >> /etc/profile.d/go.sh <<EOF
@@ -11,7 +11,7 @@ cat >> /etc/profile.d/go.sh <<EOF
 export GOROOT=/usr/local/go
 export GOPATH=\${GOROOT}/mylib
 export GOPROXY=https://goproxy.cn
-export GO111MODULE=on
+export GO111MODULE=off
 
 go_bin_path=\${GOPATH}/bin:\${GOROOT}/bin
 if [ -n "\${PATH##*\${go_bin_path}}" -a -n "\${PATH##*\${go_bin_path}:*}" ]; then
