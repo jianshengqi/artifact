01c17df7396c8611bdd3f7f1a5f1fb4e44e82c16
jianshengqi <shengqi.jian@qq.com>
2020-11-06 09:32:42 +0800

    sync

--- docker/ubuntu/scripts/protoc.sh
+++ docker/ubuntu/scripts/protoc.sh
@@ -5,6 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 curl -LO $PB_REL/download/v3.13.0/protoc-3.13.0-linux-x86_64.zip
 unzip protoc-3.13.0-linux-x86_64.zip -d $HOME/.local
+rm -f protoc-3.13.0-linux-x86_64.zip
 
 cat >> /etc/profile.d/protoc.sh <<EOF
 # shellcheck shell=sh
