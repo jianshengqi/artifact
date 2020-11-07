da9e4a46fc49fee1595965aa1bad6f9091e54b50
shengqi.jian <shengqi.jian@qq.com>
2020-11-07 18:01:03 +0800

    sync

--- virtualization/packer/ubuntu/scripts/bpf.sh
+++ virtualization/packer/ubuntu/scripts/bpf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-apt install -y build-essential git make libelf-dev clang strace tar bpfcc-tools linux-headers-$(uname -r) gcc-multilib
+apt-get install -y build-essential git make libelf-dev clang strace tar bpfcc-tools linux-headers-$(uname -r) gcc-multilib
