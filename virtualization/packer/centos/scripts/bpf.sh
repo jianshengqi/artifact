eefbf2b1c93fe028ea36c70ae09cfb7c50bc33d2
shengqi.jian <shengqi.jian@qq.com>
2020-11-07 16:46:43 +0800

    sync

---
+++ virtualization/packer/centos/scripts/bpf.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -eux
+
+dnf install make glibc-devel.i686 elfutils-libelf-devel wget tar clang bcc strace kernel-devel -y
---
+++ virtualization/packer/ubuntu/scripts/bpf.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -eux
+
+apt install build-essential git make libelf-dev clang strace tar bpfcc-tools linux-headers-$(uname -r) gcc-multilib
