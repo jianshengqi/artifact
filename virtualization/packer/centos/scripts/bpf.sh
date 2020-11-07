b79c914b4f829a48c9a1251dad740eddfddabb67
shengqi.jian <shengqi.jian@qq.com>
2020-11-07 16:53:53 +0800

    sync

--- virtualization/packer/centos/scripts/bpf.sh
+++ virtualization/packer/centos/scripts/bpf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-dnf install make glibc-devel.i686 elfutils-libelf-devel wget tar clang bcc strace kernel-devel -y
+dnf install -y make glibc-devel.i686 elfutils-libelf-devel wget tar clang bcc strace kernel-devel
