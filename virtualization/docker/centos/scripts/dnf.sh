27b3587d7182d45723048b149516fa0ed89c7cc3
shengqi.jian <shengqi.jian@qq.com>
2020-11-08 09:38:37 +0800

    sync

--- virtualization/docker/centos/scripts/dnf.sh
+++ virtualization/docker/centos/scripts/dnf.sh
@@ -5,5 +5,5 @@ if [ ! -d "/tmp" ]; then
 fi
 
 TMPDIR=/tmp
-yum install -y dnf
-dnf install -y dnf-plugins-core
+yum install -y dnf dnf-utils
+dnf install -y dnf-plugins-core redhat-lsb-core elfutils-libelf-devel
