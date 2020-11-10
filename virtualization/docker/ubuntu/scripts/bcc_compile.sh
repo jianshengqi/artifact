9a5443c24248bb09a494bcf90b841f3e35dda7c8
jianshengqi <shengqi.jian@qq.com>
2020-11-10 15:52:31 +0800

    sync

--- virtualization/docker/ubuntu/scripts/bcc_compile.sh
+++ virtualization/docker/ubuntu/scripts/bcc_compile.sh
@@ -5,9 +5,16 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y gnupg gnupg1 gnupg2 lsb-core
-
-sudo apt-get -y install bison build-essential cmake flex git libedit-dev \
-  libllvm3.7 llvm-3.7-dev libclang-3.7-dev python zlib1g-dev libelf-dev
+apt-get install -y \
+            bison \
+            build-essential \
+            cmake \
+            flex \
+            git \
+            libedit-dev \
+            libllvm3.7 \
+            llvm-3.7-dev \
+            libclang-3.7-dev python zlib1g-dev libelf-dev
 
 git clone https://github.com/iovisor/bcc.git
 mkdir bcc/build; cd bcc/build
@@ -17,5 +24,5 @@ sudo make install
 cmake -DPYTHON_CMD=python3 .. # build python3 binding
 pushd src/python/
 make
-sudo make install
+make install
 popd
