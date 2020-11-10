0c1362cd30fdfa451934d56a77706e8245718e0d
shengqi.jian <shengqi.jian@qq.com>
2020-11-10 22:10:45 +0800

    sync

--- venv/docker/ubuntu/scripts/protoc.sh
+++ venv/docker/ubuntu/scripts/protoc.sh
@@ -8,9 +8,7 @@ apt-get install -y autoconf automake libtool curl make g++ unzip
 
 PB_REL="https://github.com/protocolbuffers/protobuf/releases"
 PB_VER="3.13.0"
-curl -LO ${PB_REL}/download/${PB_VER}/protoc-${PB_VER}-linux-x86_64.zip
-ls -lh
-echo protoc-${PB_VER}-linux-x86_64.zip
+curl -LO ${PB_REL}/download/v${PB_VER}/protoc-${PB_VER}-linux-x86_64.zip
 unzip protoc-${PB_VER}-linux-x86_64.zip -d ${HOME}/.local
 rm -f protoc-${PB_VER}-linux-x86_64.zip
 
