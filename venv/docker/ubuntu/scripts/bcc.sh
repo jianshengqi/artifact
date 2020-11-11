547e84d736c7e1002f4a0d0c4da806f4e54a3e9e
jianshengqi <shengqi.jian@qq.com>
2020-11-11 12:25:41 +0800

    sync

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -5,4 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y gnupg gnupg1 gnupg2 lsb-core
-apt-get install -y bpfcc-tools linux-headers-$(uname -r)
+apt-get install -y bpfcc-tools
+# apt-get install -y linux-headers-$(uname -r)
--- venv/packer/ubuntu/scripts/bcc.sh
+++ venv/packer/ubuntu/scripts/bcc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -eux
 
-apt-get install -y build-essential git make libelf-dev clang strace tar bpfcc-tools linux-headers-$(uname -r) gcc-multilib
+apt-get install -y build-essential git make libelf-dev clang strace tar bpfcc-tools gcc-multilib
+apt-get install -y linux-headers-$(uname -r)
