5d2119a039f92d70b2f6a111eddec2ef807caec5
shengqi.jian <shengqi.jian@qq.com>
2020-11-14 19:47:34 +0800

    sync

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -7,3 +7,4 @@ apt-get upgrade -y
 apt-get install -y gnupg gnupg1 gnupg2 lsb-core
 apt-get install -y bpfcc-tools
 apt-get install -y linux-headers-$(uname -r)
+apt-get install -y build-essential git make libelf-dev clang strace tar gcc-multilib
