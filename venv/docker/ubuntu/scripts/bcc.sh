cbe2c6154f24719cb52795ab498dd7b55eca49b3
shengqi.jian <shengqi.jian@qq.com>
2020-11-15 09:56:54 +0800

    rm sudo

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -15,5 +15,5 @@ fi
 
 wget -c https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.9.tar.gz -O - | tar -xz --strip-components 1 -C /kernel-src
 cd /kernel-src/tools/lib/bpf
-make && sudo make install prefix=/usr/local
+make && make install prefix=/usr/local
 mv /usr/local/lib64/libbpf.* /lib/x86_64-linux-gnu/
