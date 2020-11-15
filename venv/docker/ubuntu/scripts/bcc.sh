8a5430354ac98fd1124f8e3784e56221d9179e24
shengqi.jian <shengqi.jian@qq.com>
2020-11-15 21:30:41 +0800

    sync

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -7,7 +7,7 @@ apt-get upgrade -y
 apt-get install -y gnupg gnupg1 gnupg2 lsb-core
 apt-get install -y bpfcc-tools
 apt-get install -y linux-headers-$(uname -r)
-apt-get install -y build-essential git make libelf-dev clang strace tar gcc-multilib
+apt-get install -y build-essential git make libelf-dev clang strace tar gcc-multilib kmod
 
 if [ ! -d "/kernel-src" ]; then
   mkdir /kernel-src
