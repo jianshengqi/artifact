ee95b39bc43979f9bc308e985bdd99bb072556ec
shengqi.jian <shengqi.jian@qq.com>
2020-11-15 11:49:26 +0800

    sync

--- venv/packer/ubuntu/scripts/bcc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-
-apt-get install -y build-essential git make libelf-dev clang strace tar bpfcc-tools gcc-multilib
-apt-get install -y linux-headers-$(uname -r)
--- venv/packer/ubuntu/scripts/cpp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-
-apt-get install -y gcc g++ gdb
-apt-get install -y llvm clang
