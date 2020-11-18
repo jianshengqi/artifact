c495c291af2b99b0d055ca1f63609184f6bebc32
shengqi.jian <shengqi.jian@qq.com>
2020-11-18 22:31:55 +0800

    sync

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -6,13 +6,12 @@ apt-get update -y
 apt-get upgrade -y
 apt-get install -y gnupg gnupg1 gnupg2 lsb-core
 apt-get install -y bpfcc-tools
-apt-get install -y build-essential git make libelf-dev clang strace tar gcc-multilib kmod
+apt-get install -y build-essential git make libelf-dev libbfd-dev clang strace tar linux-headers-$(uname -r) gcc-multilib kmod
 
-if [ ! -d "/kernel-src" ]; then
-  mkdir /kernel-src
-fi
-
-wget -c https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.0.9.tar.gz -O - | tar -xz --strip-components 1 -C /kernel-src
-cd /kernel-src/tools/lib/bpf
+SRC=/kernel-src
+git clone --depth 1 git://kernel.ubuntu.com/ubuntu/ubuntu-focal.git ${SRC}
+cd ${SRC}/tools/lib/bpf
+make && make install prefix=/usr/local
+cd ${SRC}/tools/bpf/bpftool
 make && make install prefix=/usr/local
 mv /usr/local/lib64/libbpf.* /lib/x86_64-linux-gnu/
