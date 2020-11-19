edf4777072c0613906092d8c1215b8cfd1b1ea93
jianshengqi <shengqi.jian@qq.com>
2020-11-19 20:30:07 +0800

    sync

--- venv/docker/ubuntu/scripts/bcc.sh
+++ venv/docker/ubuntu/scripts/bcc.sh
@@ -8,14 +8,14 @@ apt-get install -y gnupg gnupg1 gnupg2 lsb-core
 apt-get install -y bpfcc-tools
 apt-get install -y build-essential git make libelf-dev libbfd-dev clang strace tar gcc-multilib kmod
 
-SRC=/kernel-src
+SRC=/usr/src/ubuntu-focal
 git clone --depth 1 git://kernel.ubuntu.com/ubuntu/ubuntu-focal.git ${SRC}
 rm -rf ${SRC}/.git
 cd ${SRC}/tools/lib/bpf
 make && make install prefix=/usr/local
 cd ${SRC}/tools/bpf/bpftool
 make && make install prefix=/usr/local
-mv /usr/local/lib64/libbpf.* /lib/x86_64-linux-gnu/
+cp /usr/local/lib64/libbpf.* /lib/x86_64-linux-gnu/
 
 apt-get autoremove -y --purge
 apt-get clean
