e17e13b052d30217b65a649775a465b62bed2f95
jianshengqi <shengqi.jian@qq.com>
2020-11-20 13:33:13 +0800

    sync

--- venv/docker/ubuntu/scripts/test.sh
+++ venv/docker/ubuntu/scripts/test.sh
@@ -4,9 +4,3 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get install -y software-properties-common
-
-add-apt-repository -y ppa:sthima/oss
-apt-get update -y --allow-unauthenticated
-apt-get install -y libstapsdt0 libstapsdt-dev
-
-apt-get install -y libelf1 libelf-dev
