e332a6245785d40e30bf49ae5f6287bb81e39db3
sqjian <shengqi.jian@qq.com>
2020-12-07 09:50:32 +0800

    sync

--- docker/ubuntu/scripts/env.sh
+++ docker/ubuntu/scripts/env.sh
@@ -16,3 +16,20 @@ tee -a /root/.bashrc <<-'EOF'
 # reload system profile
 . /etc/profile
 EOF
+
+tee /etc/apt/sources.list <<- 'EOF'
+deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+EOF
