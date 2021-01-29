fade8849600723071189a3b3225001abc4f8120c
sqjian <shengqi.jian@qq.com>
2021-01-29 10:27:09 +0800

    sync

--- packer/vagrantfile/scripts/bashrc.sh
+++ packer/vagrantfile/scripts/bashrc.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
 tee -a /etc/bash.bashrc <<-'EOF'
-export http_proxy="socks5://10.7.41.6:7891"
-export https_proxy="socks5://10.7.41.6:7891"
-EOF
-
-tee -a /etc/bash.bashrc <<-'EOF'
-alias sync='rsync -av --progress --delete /vagrant/ ~/dev_box'
+alias sync='rsync -av --progress --delete /vagrant/ ~/box'
 EOF
