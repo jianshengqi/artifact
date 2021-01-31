b16c909b600db94ca911b167fae8968ff8961c3b
shengqi.jian <shengqi.jian@qq.com>
2021-01-31 12:45:02 +0800

    sync

--- docker/ubuntu/scripts/tmux.sh
+++ docker/ubuntu/scripts/tmux.sh
@@ -29,13 +29,23 @@ rm -rf \
 
 tee /etc/tmux.conf <<-'EOF'
 # 基础设置
-set -g default-terminal "screen-256color"  # 设置终端缺省的支持颜色为256色
-set -g default-shell /usr/bin/bash # 设置默认shell
-set -g base-index 1                # 窗口编号从 1 开始计数
-set -g pane-base-index 1           # 窗格编号从 1 开始计数
-setw -g allow-rename on           # 禁止活动进程修改窗口名
-setw -g mode-keys vi               # 复制模式中的默认键盘布局；可以设置为vi或emacs
-set-option -g mouse on             # 开启鼠标支持
+set-option -g default-shell /usr/bin/bash
+set-option -g default-terminal "screen-256color"           # 设置终端缺省的支持颜色为256色
+set-option -g base-index 1                                 # 窗口编号从 1 开始计数
+set-option -g pane-base-index 1                            # 窗格编号从 1 开始计数
+set-option -g mouse on                                     # 开启鼠标支持
+set-option -g visual-activity on                           # 开启窗口操作的可视
+set-option -g set-clipboard on                             # 开启剪切板
+set-option -g display-panes-time 800                       # 稍长的窗格中显示的时间指标，单位为毫秒
+set-option -g display-time 1000                            # 提示信息的持续时间；设置足够的时间以避免看不清提示，单位为毫秒
+set-option -g repeat-time 1000                             # 控制台激活后的持续时间；设置合适的时间以避免每次操作都要先激活控制台，单位为毫秒
+set-option -g renumber-windows on                          # 关掉某个窗口后，编号重排
+set-window-option -g automatic-rename on                   # 重命名窗口，以反映当前的程序
+set-window-option -g monitor-activity on                   # 开启窗口活动监视
+set-window-option -g clock-mode-style 24                   # 24小时显示方式
+set-window-option -g allow-rename on                       # 禁止活动进程修改窗口名
+set-window-option -g mode-keys vi                          # 复制模式中的默认键盘布局；可以设置为vi或emacs
+set-window-option -g mouse on                              # 窗口切换后让人可以用鼠标上下滑动显示历史输出
 
 # 绑定hjkl键为面板切换的上下左右键
 bind -r k select-pane -U # 绑定k为↑,选择上面板
@@ -54,21 +64,18 @@ bind ^u swapp -U # 与上面板交换
 bind ^d swapp -D # 与下面板交换
 
 # 窗格边框
-set -g pane-border-style fg='#747474',bg=default        # 设置边界颜色（bg背景颜色， fg为线条颜色）
-set -g pane-active-border-style fg='magenta',bg=default # 设置正在使用的窗口的边界颜色，在不同窗口切换时边界颜色会变化
+set-option -g pane-border-style fg='#747474',bg=default        # 设置边界颜色（bg背景颜色， fg为线条颜色）
+set-option -g pane-active-border-style fg='magenta',bg=default # 设置正在使用的窗口的边界颜色，在不同窗口切换时边界颜色会变化
 
 # 状态栏
-set -g status-style fg=white,bg=default       # 底部命令或者状态栏的颜色
-set -g status-interval 1                      # 状态栏的刷新时间间隔
-set -g status-keys vi                         # 操作状态栏时的默认键盘布局；可以设置为vi或emacs
-set -g visual-activity on                     # 开启窗口操作的可视
-set -g set-clipboard on                       # 开启剪切板
-set -g display-panes-time 800                 # 稍长的窗格中显示的时间指标
-set -g display-time 1000                      # 稍长的状态消息的显示时间，单位为毫秒
-set -g renumber-windows on                    # 关掉某个窗口后，编号重排
-setw -g automatic-rename on                                                                                                                                                     # 重命名窗口，以反映当前的程序
-setw -g monitor-activity on                                                                                                                                                     # 开启窗口活动监视
-setw -g clock-mode-style 24                                                                                                                                                     # 24小时显示方式
-setw -g window-status-current-format '#[fg=yellow,bold]** #{window_index} #[fg=yellow]#{pane_current_command} #[fg=yellow]#(echo "#{pane_current_path}" | rev | cut -d'/' -f-3 | rev) #[fg=yellow]** #[fg=white]|' # 活动窗格状态栏色彩
-setw -g window-status-format '#[fg=white]#{window_index} #[fg=white]#{pane_current_command} #[fg=white]#(echo "#{pane_current_path}" | rev | cut -d'/' -f-3 | rev) #[fg=white]|'                                   # 活动窗格状态栏色彩
+set-option -g status-justify "left"                                    # 窗口列表居中显示
+set-option -g status-left '[#{session_name}]'                          # 状态栏左方的内容
+set-option -g status-right '[#[fg=white]#(date +" %m-%d %H:%M ")]'     # 状态栏右方的内容；这里的设置将得到类似23:59的显示
+set-option -g status-left-length 10                                    # 状态栏左方的内容长度；
+set-option -g status-right-length 15                                   # 状态栏右方的内容长度；建议把更多的空间留给状态栏左方（用于列出当前窗口）
+set-option -g status-style fg=white,bg=default                         # 底部命令或者状态栏的颜色
+set-option -g status-interval 1                                        # 状态栏的刷新时间间隔
+set-option -g status-keys vi                                           # 操作状态栏时的默认键盘布局；可以设置为vi或emacs
+set-window-option -g window-status-current-format '#[fg=yellow,bold]** #{window_index} #[fg=yellow]#{pane_current_command} #[fg=yellow]#(echo "#{pane_current_path}" | rev | cut -d'/' -f-3 | rev) #[fg=yellow]** #[fg=white]' # 活动窗格状态栏色彩
+set-window-option -g window-status-format '#[fg=white]#{window_index} #[fg=white]#{pane_current_command} #[fg=white]#(echo "#{pane_current_path}" | rev | cut -d'/' -f-3 | rev) #[fg=white]'                                   # 活动窗格状态栏色彩
 EOF
