2d98c8d8a91c567f3d5039be97a3d59b9dcedc1d
sqjian <shengqi.jian@qq.com>
2021-02-07 19:32:26 +0800

    sync

--- packer/ubuntu/scripts/ripgrep.sh
+++ packer/ubuntu/scripts/ripgrep.sh
@@ -7,8 +7,7 @@ if [ ! -d ${WORKDIR} ]; then
     mkdir -p ${WORKDIR}
 fi
 
-pushd ${WORKDIR}
+cd ${WORKDIR}
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
 dpkg -i ripgrep_12.1.1_amd64.deb
 rm -rf *.deb
-popd
