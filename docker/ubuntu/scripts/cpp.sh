7cd3336eb13f19606653f9d5d266439dcb38163e
sqjian <shengqi.jian@qq.com>
2021-05-17 09:26:16 +0800

    Update cpp.sh

--- docker/ubuntu/scripts/cpp.sh
+++ docker/ubuntu/scripts/cpp.sh
@@ -5,20 +5,9 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y \
-            build-essential \
             gcc \
             g++ \
-            gdb \
-            make \
-            automake \
-            cmake \
-            autoconf \
-            llvm \
-            clang
-
-# usdt lib:systemtap-sdt-dev
-apt-get install -y \
-            systemtap-sdt-dev
+            make
 
 apt-get autoremove -y --purge
 apt-get clean
