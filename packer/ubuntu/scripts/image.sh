ac2b0ca8af033239ae5bbd4bc90a3f68eb0a6041
sqjian <shengqi.jian@gmail.com>
2021-05-27 00:39:58 +0800

    sync image.sh

---
+++ packer/ubuntu/scripts/image.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -eux
+export DEBIAN_FRONTEND=noninteractive
+
+# Update the package list
+apt-get -y update;
+
+# Upgrade kernel to 5.8
+apt-get install -y linux-image-5.8.0-28-generic
+
+reboot
