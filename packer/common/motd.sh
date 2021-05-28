f5ff82f21741adbf6bf899a881ff4e50ec7b0e79
sqjian <shengqi.jian@qq.com>
2021-05-28 15:19:37 +0800

    sync motd

--- packer/common/motd.sh
+++ packer/common/motd.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -eux
 
-doge="This system is built by doge in $(date '+%Y-%m-%d %H:%M:%S')"
+motd="This system is built by sqjian in $(date '+%Y-%m-%d %H:%M:%S')"
 
 if [ -d /etc/update-motd.d ]; then
-    MOTD_CONFIG='/etc/update-motd.d/99-doge'
+    MOTD_CONFIG='/etc/update-motd.d/99-motd'
 
-    cat >> "$MOTD_CONFIG" <<DOGE
+    cat >> "$MOTD_CONFIG" <<MOTD
 #!/bin/sh
 
 cat <<'EOF'
-$doge
+$motd
 
 EOF
-DOGE
+MOTD
 
     chmod 0755 "$MOTD_CONFIG"
 else
-    echo "$doge" >> /etc/motd
+    echo "$motd" >> /etc/motd
 fi
