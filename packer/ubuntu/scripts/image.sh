c76703963605af99648204c91f8790666ea2bb14
sqjian <shengqi.jian@qq.com>
2021-06-01 13:35:11 +0800

    Update image.sh

--- packer/ubuntu/scripts/image.sh
+++ packer/ubuntu/scripts/image.sh
@@ -6,6 +6,6 @@ apt-get -y update;
 
 # Upgrade kernel to 5.8
 # apt-cache search linux-image
-apt-get install -y linux-image-5.8.0-28-generic
+apt-get install -y linux-image-5.10.0-1008-oem
 
 reboot
