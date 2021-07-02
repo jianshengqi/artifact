e3a8c6782f76d4791931c879d062f0e42bdf3e69
sqjian <shengqi.jian@qq.com>
2021-07-02 17:54:21 +0800

    sync

--- packer/common/motd.sh
+++ packer/common/motd.sh
@@ -1,13 +1,31 @@
 #!/bin/sh -eux
 
-motd="This system is built by sqjian in $(date '+%Y-%m-%d %H:%M:%S')"
+motd1=$(cat <<- 'EOF'
+    / \__
+   (    @\___
+   /         O
+  /   (_____/
+ /_____/   U
+EOF
+)
+
+motd2=$(cat <<- EOF
+This system is built by sqjian in $(date '+%Y-%m-%d %H:%M:%S')
+EOF
+)
+
+motd=$(cat <<- EOF
+$motd1
+
+$motd2
+EOF
+)
 
 if [ -d /etc/update-motd.d ]; then
     MOTD_CONFIG='/etc/update-motd.d/99-motd'
 
-    cat >> "$MOTD_CONFIG" <<MOTD
+    cat > "$MOTD_CONFIG" <<MOTD
 #!/bin/sh
-
 cat <<'EOF'
 $motd
 
@@ -16,5 +34,5 @@ MOTD
 
     chmod 0755 "$MOTD_CONFIG"
 else
-    echo "$motd" >> /etc/motd
+    echo "$motd" > /etc/motd
 fi
